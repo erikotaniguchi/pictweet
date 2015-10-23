4c1ec315e41cdde145c8c2ce7f5de0283b4ee4c2
TaniguchiEriko <taniguchieriko@TaniguchiEriko-no-MacBook-Air.local>
2015-10-23 17:28:50 +0900

    edit migration file

--- db/migrate/20151023082215_create_tweets.rb
+++ db/migrate/20151023082215_create_tweets.rb
@@ -1,8 +1,11 @@
 class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
+      t.string            :name
+      t.text              :text
+      t.text              :image
 
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
